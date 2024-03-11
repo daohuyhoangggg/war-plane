ea2aae4e01f253f032ed51f475b76ff5c47419a3
daohuyhoangggg <23020666@vnu.edu.vn>
2024-03-11 22:45:23 +0700

    initial commit

---
+++ war plane/thuvien.cpp
@@ -0,0 +1,39 @@
+﻿
+#include"thuvien.h"
+
+SDL_Surface* SDLCommonFunc::LoadImage(std::string file_path)
+{
+	SDL_Surface* loadImage = NULL;				// lưu trữ hình ảnh từ file
+	SDL_Surface* optimizeImage = NULL;			// lưu trữ hình ảnh được tối ưu hóa
+
+	loadImage = IMG_Load(file_path.c_str());
+
+	if(loadImage != NULL)
+	{
+		optimizeImage = SDL_DisplayFormat(loadImage);			// tối ưu hóa hình ảnh
+		SDL_FreeSurface(loadImage);
+
+		if(optimizeImage != NULL)
+		{
+				// loại bỏ background của cái đối tượng con
+				UINT32 color_key = SDL_MapRGB(optimizeImage->format, 0, 0xFF, 0xFF);		
+				SDL_SetColorKey(optimizeImage, SDL_SRCCOLORKEY, color_key);
+
+		}
+	}
+	return optimizeImage;
+}
+
+void SDLCommonFunc::ApplySurface(SDL_Surface* src, SDL_Surface* des, int x, int y)				// vẽ lên tọa độ cụ thể
+{
+	SDL_Rect offset;
+	offset.x = x;
+	offset.y = y;
+	SDL_BlitSurface(src, NULL, des, &offset);
+}
+
+void SDLCommonFunc::CleanUp()
+{
+	SDL_FreeSurface(gScreen);
+	SDL_FreeSurface(gBkground);
+}
---
+++ war plane/thuvien.h
@@ -0,0 +1,27 @@
+﻿#ifndef COMMON_FUNCTION_H_
+#define COMMON_FUNCTION_H_
+
+#include<windows.h>
+#include<SDL.h>
+#include<string>
+#include<SDL_image.h>
+
+
+
+const int SCREEN_WIDTH = 1200;
+const int SCREEN_HEIGHT = 675;
+const int SCREEN_BPP = 32;
+
+
+static SDL_Surface* gScreen = NULL;
+static SDL_Surface* gBkground = NULL;
+static SDL_Event gEven;
+
+namespace SDLCommonFunc
+{
+	SDL_Surface* LoadImage(std::string file_path);				// tải ảnh 
+	void ApplySurface(SDL_Surface* src, SDL_Surface* des, int x, int y);    // vẽ ảnh src lên des(màn hinh) tại tọa độ(x,y)
+	void CleanUp();					// Giải phóng tài nguyên
+}
+
+#endif
